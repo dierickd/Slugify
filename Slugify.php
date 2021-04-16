096dacf426e46f4c20b5e19658e70f49e0529dbd
dierickd <37022827+dierickd@users.noreply.github.com>
2021-04-16 20:42:11 +0200

    Update Slugify.php

--- Slugify.php
+++ Slugify.php
@@ -1,40 +1,46 @@
 <?php
 
 
-namespace App\Service;
+namespace App\Services;
 
 
 class Slugify
 {
-    public function generate(string $string): string
-    {
 
-        $specialsCharacters = [':','!','?','/','\\',';',',','.','§','*','%','$',')','(',']','[','&','=','#','"','|','{','}'];
-        $specialsA = ['à','á','â','ã','ä','å'];
-        $specialsC = ['ç'];
-        $specialsO = ['ð','ò','ó','ô','õ','ö'];
-        $specialsU = ['ù','ú','û','ü'];
-        $specialsI = ['ì','í','î','ï'];
-        $specialsY = ['ý','ÿ'];
-        $specialsE = ['è','é','ê','ë','É'];
+   public function generate(string $string, string $separator = "-"): string
+   {
+      $separators = ["-", "_", "*"];
 
-        $slug = str_replace($specialsCharacters, '', $string);
-        $slug = str_replace($specialsA, 'a', $slug);
-        $slug = str_replace($specialsC, 'c', $slug);
-        $slug = str_replace($specialsO, 'o', $slug);
-        $slug = str_replace($specialsU, 'u', $slug);
-        $slug = str_replace($specialsI, 'i', $slug);
-        $slug = str_replace($specialsY, 'y', $slug);
-        $slug = str_replace($specialsE, 'e', $slug);
-        $slug = str_replace('-', ' ', $slug);
+      if (!in_array($separator, $separators, true)) {
+         $separator = "-";
+      }
 
-        $slug = str_replace('\'', '-', $slug);
+      $specialsCharacters = [':', '!', '?', '/', '\\', ';', ',', '.', '§', '*', '%', '$', ')', '(', ']', '[', '&', '=', '#', '"', '|', '{', '}'];
+      $specialsA = ['à', 'á', 'â', 'ã', 'ä', 'å'];
+      $specialsC = ['ç'];
+      $specialsO = ['ð', 'ò', 'ó', 'ô', 'õ', 'ö'];
+      $specialsU = ['ù', 'ú', 'û', 'ü'];
+      $specialsI = ['ì', 'í', 'î', 'ï'];
+      $specialsY = ['ý', 'ÿ'];
+      $specialsE = ['è', 'é', 'ê', 'ë', 'É'];
 
-        $arr = explode(' ', $slug);
-        $arr = array_map('trim', $arr);
-        $arr = array_filter($arr);
-        $slug = implode('-',$arr);
+      $slug = str_replace($specialsCharacters, '', $string);
+      $slug = str_replace($specialsA, 'a', $slug);
+      $slug = str_replace($specialsC, 'c', $slug);
+      $slug = str_replace($specialsO, 'o', $slug);
+      $slug = str_replace($specialsU, 'u', $slug);
+      $slug = str_replace($specialsI, 'i', $slug);
+      $slug = str_replace($specialsY, 'y', $slug);
+      $slug = str_replace($specialsE, 'e', $slug);
+      $slug = str_replace('-', ' ', $slug);
 
-        return strtolower($slug);
-    }
+      $slug = str_replace('\'', '-', $slug);
+
+      $arr = explode(' ', $slug);
+      $arr = array_map('trim', $arr);
+      $arr = array_filter($arr);
+      $slug = implode($separator, $arr);
+
+      return strtolower($slug);
+   }
 }
